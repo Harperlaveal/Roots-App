b9177e1075c76ac22d0196f8ffbb7d002421031d
Eddy Horwood <eddyhorwood@gmail.com>
2022-08-30 12:54:25 -0700

    Have Community working
    Final cosmetic touches may be required, also need to implement firebase interaction for scores and names of users

--- RootsApp/components/screens/Community.js
+++ RootsApp/components/screens/Community.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { StyleSheet, View, Text, Button, Alert } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import Leaderboard from './Leaderboard';
-import Profiles from './Profiles';
 
 
 export default function Community({ navigation }) {
@@ -10,42 +9,23 @@ export default function Community({ navigation }) {
 
         <View style={styles.background}>
             <View style={styles.title}>
-                <Text style={styles.titleText}>Title</Text>
+                <Text style={styles.titleText}>Community</Text>
             </View>
-            <View style={styles.profiles}>
-                <ScrollView horizontal={true} contentContainerStyle={{
-          flexGrow: 1,
-          justifyContent: 'flex-end',
-      }}>
-                    <View style={styles.container, { flexDirection: 'row', justifyContent: 'flex-end' }}>
-                        <View>
-                            <Profiles name="Eddy" score={100} />
-                        </View>
-                        <View>
-                            <Profiles name="Eddy" score={12} />
-                        </View>
-                        <View>
-                            <Profiles name="Eddy" score={55} />
-                        </View>
-                    </View>
-                </ScrollView>
+            <View style={styles.LeaderboardContainer}>
+                <Leaderboard></Leaderboard>
             </View>
+
         </View>
 
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 20,
-    },
     text: {
         fontSize: 26,
         fontWeight: 'bold'
     },
     title: {
-
         fontSize: 30,
         fontWeight: 'bold'
     },
@@ -61,11 +41,9 @@ const styles = StyleSheet.create({
         fontSize: 40,
         fontWeight: "bold"
     },
-    profiles: {
-        
+    LeaderboardContainer: {
         position: 'absolute',
         bottom: 0,
-        flex: 2,
     }
 
 })
