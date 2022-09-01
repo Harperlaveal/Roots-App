f47515c6a30d9a3b637f261c06790c5f65a662db
Harper Doak <doakharp@ecs.vuw.ac.nz>
2022-09-01 19:14:32 +1200

    Added icons that can be pressed to map

--- RootsApp/components/screens/MapScreen.js
+++ RootsApp/components/screens/MapScreen.js
@@ -1,18 +1,49 @@
 import * as React from 'react';
 import MapView from 'react-native-maps';
-import { StyleSheet, View, Text, Dimensions } from 'react-native';
+import { Marker } from 'react-native-maps';
+import { StyleSheet, View, Text, Dimensions, Image } from 'react-native';
+
 
 export default function MapScreen()  {
 
-    
     return (
-        //Starts in NZ now
-        //Need to workout how to start zoomed into wellington because
-        //it wasn't working for some reason.
+       //Need to suss custom images out.
     <View style ={styles.container}>
-        <MapView
-        initialRegion={{ latitude: -41.276825, longitude: 174.777969, latitudeDelta: 0.03, longitudeDelta: 0.03 }}
-            style={styles.map}> 
+        <MapView style={styles.map}
+        initialRegion={{ 
+            latitude: -41.276825, 
+            longitude: 174.777969, 
+            latitudeDelta: 0.03, 
+            longitudeDelta: 0.03 
+        }}
+        > 
+        <Marker
+            coordinate={{latitude: -41.27516,
+            longitude: 174.76680}}
+            title={"Tree Planting 101"}
+            description={"Click to sign-up"}
+        />
+        <Marker
+            coordinate={{latitude: -41.29838,
+            longitude: 174.75219}}
+            pinColor = {"green"}
+            title={"Tree Planting 201"}
+            description={"Click to sign-up"}
+        />
+        <Marker
+            coordinate={{latitude: -41.29042,
+            longitude: 174.79066}}
+            pinColor = {"red"}
+            title={"Oriental Bay Rubbish Pick up "}
+            description={"Click to sign-up"}
+        />
+         <Marker
+            coordinate={{latitude: -41.33006,
+            longitude: 174.79410}}
+            pinColor = {"red"}
+            title={"Lyall Bay Clean up"}
+            description={"Click to sign-up"}
+        />
         </MapView>
     </View>
     );
