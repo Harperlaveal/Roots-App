5f7947d76b4848be2e206ad0d5991be3d4a65852
Gareth McIntosh <gareth.mcintosh11@gmail.com>
2022-09-01 15:43:54 +1200

    creating login page

--- RootsApp/App.js
+++ RootsApp/App.js
@@ -3,10 +3,16 @@ import React from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import { firebaseConfig } from './firebase.js';
 import { initializeApp } from 'firebase/app';
-
+import { Provider } from 'react-native-paper'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 import MainContainer from './navigation/maincontainer';
+import LoginScreen from './components/screens/LoginScreen.js';
+import { theme } from './components/theme.js';
+import { user } from './user.json';
 
 export const app = initializeApp(firebaseConfig);
+const Stack = createStackNavigator();
 
 export default class App extends React.Component {
   constructor () {
@@ -24,13 +30,28 @@ export default class App extends React.Component {
   render() {
 
     const statusbar = (Platform.os == 'ios') ? <View style={styles.statusbar}></View> : <View></View>;
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.statusbar}></View>
-      <MainContainer/>
-    </View>
-  );
+    if(user.email !== "") {
+      return (
+        <View style={styles.container}>
+          <View style={styles.statusbar}></View>
+          <MainContainer/>
+        </View>
+      );
+    } else {
+      return(
+      <Provider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="LoginScreen"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+      </Provider>)
+    }
  }
 }
 
