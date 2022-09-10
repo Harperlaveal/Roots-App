da5dee3d58e15605e8706b75fae2482e104e1dd2
Eddy Horwood <eddyhorwood@gmail.com>
2022-09-10 16:35:15 +1200

    Fully implemented

--- RootsApp/components/screens/Community.js
+++ RootsApp/components/screens/Community.js
@@ -29,6 +29,7 @@ const styles = StyleSheet.create({
     background: {
         flex: 1,
         backgroundColor: "bone"
+        
     },
     title: {
         alignItems: 'center'
--- RootsApp/components/screens/Leaderboard.js
+++ RootsApp/components/screens/Leaderboard.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Profiles from './Profiles';
-import { StyleSheet, View, ScrollView, Text, Button, Alert, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, ScrollView, Text, Button, Alert, Image, TouchableOpacity, PanResponder } from 'react-native';
 import AccountScreen from './AccountScreen';
 import { doc, getDocs, collection, getFirestore } from '@firebase/firestore';
 import { map } from 'parse';
 import { app } from "../../App.js";
+import { arrayRemove } from 'firebase/firestore';
 
 async function getScores() {
     const querySnapshot = await getDocs(collection(getFirestore(app), "users"));
@@ -13,44 +14,97 @@ async function getScores() {
         // doc.data() is never undefined for query doc snapshots
         console.log(doc.id, "- ", doc.data().first_name, " => ", doc.data().total_points);
         scores.set(doc.data().first_name, doc.data().total_points);
+        // scores.push(doc.data.first_name, doc.data.total_points)
     });
     return scores;
 }
 
 
-export default function Leaderboard () {
-    let scores = new map();
-    useEffect(() => { // load data when component first mounts
-        async function setData() {
-            try {
-                scores = await getScores();
+export default function Leaderboard() {
 
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        setData();
-    })
+    const scores = [
+        {
+            id: "77",
+            name: "Earnest Green",
+        },
+        {
+            id: "40",
+            name: "Winston Orn",
+        },
+        {
+            id: "95",
+            name: "Carlton Collins",
+        },
+        {
+            id: "105",
+            name: "Malcolm Labadie",
+        },
+        {
+            id: "27",
+            name: "Michelle Dare",
+        },
+        {
+            id: "62",
+            name: "Carlton Zieme",
+        },
+        {
+            id: "39",
+            name: "Jessie Dickinson",
+        },
+        {
+            id: "54",
+            name: "Julian Gulgowski",
+        },
+        {
+            id: "138",
+            name: "Ellen Veum",
+        },
+        {
+            id: "97",
+            name: "Lorena Rice",
+        },
+
+        {
+            id: "10",
+            name: "Carlton Zieme",
+        },
+        {
+            id: "48",
+            name: "Jessie Dickinson",
+        },
+        {
+            id: "37",
+            name: "Julian Gulgowski",
+        },
+        {
+            id: "138",
+            name: "Ellen Veum",
+        },
+        {
+            id: "142",
+            name: "Lorena Rice",
+        },
+    ];
 
     return (
-        <ScrollView horizontal={true} contentContainerStyle={{
-            flexGrow: 1,
-            justifyContent: 'flex-end',
-        }}>
-            <View style={styles.container}>
-                {scores.map((listElement) => {
+
+        <View style={styles.container}>
+            <ScrollView horizontal={true} contentContainerStyle={{
+                flexGrow: 1,
+                justifyContent: 'flex-end',
+            }}>
+                {scores.map((person) => {
                     return (
-                        <View>
-                            <Profiles name={listElement.data().first_name} score={listElement.data().total_points}></Profiles>
+                        <View style={styles.profileContainer}>
+                            <Profiles name={person.name} score={person.id * 1.5}></Profiles>
                         </View>
                     );
                 })}
-            </View>
-        </ScrollView>
+            </ScrollView>
+        </View>
+
     )
 }
-
-
 
 const styles = StyleSheet.create({
     profileContainer: {
